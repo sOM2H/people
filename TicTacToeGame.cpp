44ce38ee2e43dca022431e0c81d639d0b0891f85
notdatboi <noddatboi@gmail.com>
2018-11-28 14:53:00 +0200

    Added JSON

--- TicTacToeGame.cpp
+++ TicTacToeGame.cpp
@@ -2,7 +2,8 @@
 
 void TicTacToe::run()
 {
-    std::cout << finished();
+    //std::cout << finished();
+    nlohmann::json allMoves(nlohmann::json::value_t::array);
     while(finished() == 'n')
     {
         field.saveToFile("Player1/field.txt");
@@ -12,65 +13,77 @@ void TicTacToe::run()
         int r, c;
         move >> r >> c;
         move.close();
+        nlohmann::json jsonMove = {r, c};
+        allMoves.push_back(jsonMove);
         try
         {
             field.setAt(r, c, 1);
         }
         catch(TicTacToeField::OutOfBounds obex)
         {
-            std::cout << "Player 1 caused exception: " << obex.what();
+            std::cerr << "Player 1 caused exception: " << obex.what();
             return;
         }
         catch(TicTacToeField::RestrictedModifying rmex)
         {
-            std::cout << "Player 1 caused exception: " << rmex.what();
+            std::cerr << "Player 1 caused exception: " << rmex.what();
             return;
         }
-        field.print();
+        //field.print();
         char result = finished();
         if(result == 'd')
         {
-            std::cout << "Draw\n";
-            return;
+        //    std::cout << "Draw\n";
+            //return;
+            break;
         }
         if(result == 'w')
         {
-            std::cout << "Player 1 won\n";
-            return;
+        //    std::cout << "Player 1 won\n";
+            //return;
+            break;
         }
         field.saveToFile("./Player2/field.txt");
         system("./Player2/run");
         move.open("./Player2/move.txt");
         move >> r >> c;
         move.close();
+        jsonMove = {r, c};
+        allMoves.push_back(jsonMove);
         try
         {
             field.setAt(r, c, 2);
         }
         catch(TicTacToeField::OutOfBounds obex)
         {
-            std::cout << "Player 2 caused exception: " << obex.what();
+            std::cerr << "Player 2 caused exception: " << obex.what();
             return;
         }
         catch(TicTacToeField::RestrictedModifying rmex)
         {
-            std::cout << "Player 2 caused exception: " << rmex.what();
+            std::cerr << "Player 2 caused exception: " << rmex.what();
             return;
         }
-        field.print();
+        //field.print();
         result = finished();
         if(result == 'd')
         {
-            std::cout << "Draw\n";
-            return;
+            //std::cout << "Draw\n";
+            //return;
+            break;
         }
         if(result == 'w')
         {
-            std::cout << "Player 2 won\n";
-            return;
+            //std::cout << "Player 2 won\n";
+            //return;
+            break;
         }
     }
-    std::cout << "Draw\n";
+    std::ofstream movesList;
+    movesList.open("moves.json");
+    movesList << allMoves.dump();
+    movesList.close();
+    //std::cout << "Draw\n";
 }
 
 char TicTacToe::finished()
--- TicTacToeGame.hpp
+++ TicTacToeGame.hpp
@@ -2,6 +2,7 @@
 #define TIC_TAC_TOE_HPP
 
 #include"TicTacToeField.hpp"
+#include<nlohmann/json.hpp>
 
 class TicTacToe
 {
